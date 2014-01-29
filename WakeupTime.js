cabcf98ed5cb85eca04fffa426c2dccb6cda7d0a
Karin <karin.j.alvarez@gmail.com>
2014-01-29 12:27:35 -0700

    Second commit

--- WakeupTime.js
+++ WakeupTime.js
@@ -1,13 +1,22 @@
 $(document).on('ready', function() {
 	var innerShell = $('<div>I am the inner shell</div>');
 	var outerShell = $('<div>I am the outer shell</div>');
-	
+	var pmLabel = $('<div class= "labelStyle">PM</div>');
+	var autoLabel = $('<div class= "labelStyleAuto">AUTO</div>');
+	var clockScreen = $('<div class= "labelStyleScreen">12:00</div>');
+	var amPMlabel = $('<div class="dotStyle">DOT</div>');
 
 		$('.container').append(innerShell);
 		$(innerShell).addClass("innerShellstyle");
 
 		$('.container').after(outerShell);
 		$(outerShell).addClass("outerShellstyle");
+
+		$(innerShell).append(pmLabel);
+		$(innerShell).append(autoLabel);
+		$(pmLabel).after(clockScreen);
+		$(autoLabel).after(amPMlabel);
+
 
 		
 
